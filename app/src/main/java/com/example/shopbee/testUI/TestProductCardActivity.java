9908697868a7a470b3ac4ba1bad07aa2163d5b74
minhluan85 <nmluan22@apcs.fitus.edu.vn>
2024-07-23 23:39:32 +0700

    Complete Rating Bar
    Use 2 xml to set full star and empty star in ScaleRatingBar
    star_empty_icon.xml
    star_full_icon.xml

---
+++ app/src/main/java/com/example/shopbee/testUI/TestProductCardActivity.java
@@ -0,0 +1,17 @@
+package com.example.shopbee.testUI;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.shopbee.R;
+
+public class TestProductCardActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.product_card);
+
+
+    }
+}
