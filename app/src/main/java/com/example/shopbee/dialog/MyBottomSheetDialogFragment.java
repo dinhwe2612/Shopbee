ea59ce37fdbafa912684427873bfc6c81e82b186
Mmyxxan <127069162+Mmyxxan@users.noreply.github.com>
2024-07-26 22:07:09 +0700

    filters layout

---
+++ app/src/main/java/com/example/shopbee/dialog/MyBottomSheetDialogFragment.java
@@ -0,0 +1,48 @@
+package com.example.shopbee.dialog;
+
+import android.app.Dialog;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.shopbee.R;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+public class MyBottomSheetDialogFragment extends BottomSheetDialogFragment {
+    BottomSheetDialog dialog;
+    BottomSheetBehavior<View> bottomSheetBehavior;
+    View rootView;
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        dialog = (BottomSheetDialog) super.onCreateDialog(savedInstanceState);
+        return dialog;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.bottom_sheet_layout, container, false);
+        return rootView;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        bottomSheetBehavior = BottomSheetBehavior.from((View) view.getParent());
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+
+        LinearLayout layout = dialog.findViewById(R.id.linearLayout);
+        assert  layout != null;
+//        layout.setMinimumHeight(Resources.getSystem().getDisplayMetrics().heightPixels);
+    }
+}
