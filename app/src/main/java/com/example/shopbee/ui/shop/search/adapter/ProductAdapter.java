0dd9badaa1528b5856e40317c5874dc085526a40
Mmyxxan <127069162+Mmyxxan@users.noreply.github.com>
2024-08-27 15:33:58 +0700

    search by category page

--- app/src/main/java/com/example/shopbee/ui/shop/search/adapter/ProductAdapter.java
+++ app/src/main/java/com/example/shopbee/ui/shop/search/adapter/ProductAdapter.java
@@ -1,5 +1,7 @@
 package com.example.shopbee.ui.shop.search.adapter;
 
+import android.graphics.Bitmap;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -7,14 +9,24 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.FutureTarget;
 import com.example.shopbee.data.model.api.AmazonProductByCategoryResponse;
 import com.example.shopbee.databinding.CategoriesShopNewItem1Binding;
 import com.example.shopbee.databinding.ShopItemBinding;
 import com.example.shopbee.ui.shop.adapter.CategoriesAdapter;
+import com.example.shopbee.ui.shop.categories.CategoriesHashMap;
 
 import java.util.List;
 
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Completable;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
 public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ViewHolder> {
+    private final CompositeDisposable compositeDisposable = new CompositeDisposable();
     private List<AmazonProductByCategoryResponse.Data.Product> products;
     public ProductAdapter(List<AmazonProductByCategoryResponse.Data.Product> products) {
         this.products = products;
@@ -49,6 +61,30 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ViewHold
         }
         public void bindView(int position) {
             binding.textView2.setText(products.get(position).getProduct_title());
+            binding.simpleRatingBar.setRating(Float.parseFloat(products.get(position).getProduct_star_rating()));
+            binding.textView3.setText(products.get(position).getProduct_num_ratings());
+            binding.textView1.setText(products.get(position).getProduct_price());
+            compositeDisposable.add(Observable.fromCallable(() -> {
+                                FutureTarget<Bitmap> futureTarget = Glide.with(binding.imageView.getContext())
+                                        .asBitmap()
+                                        .load(products.get(position).getProduct_photo())
+                                        .submit();
+                                return futureTarget.get();
+                            })
+                            .subscribeOn(Schedulers.io())
+                            .observeOn(AndroidSchedulers.mainThread())
+                            .subscribe(bitmap -> {
+                                binding.imageView.setImageBitmap(bitmap);
+                            }, throwable -> {
+                                Log.e("Exception", throwable.getMessage());
+                            })
+            );
         }
+    }
+
+    @Override
+    public void onViewDetachedFromWindow(@NonNull ViewHolder holder) {
+        super.onViewDetachedFromWindow(holder);
+        compositeDisposable.clear();
     }
 }
