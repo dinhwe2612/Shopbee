246e682796b3b5eb58565a3a4aa99f8c61651aff
dinhwe2612 <116061849+dinhwe2612@users.noreply.github.com>
2024-08-27 16:52:02 +0700

    Update: integrate DialogsEventBus into DialogsManager and remove it

--- app/src/main/java/com/example/shopbee/ui/common/dialogs/DialogsManager.java
+++ app/src/main/java/com/example/shopbee/ui/common/dialogs/DialogsManager.java
@@ -6,15 +6,38 @@ import androidx.fragment.app.FragmentManager;
 
 import com.example.shopbee.ui.common.dialogs.twooptiondialog.TwoOptionDialog;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class DialogsManager {
     Context context;
     final FragmentManager fragmentManager;
+    public interface Listener {
+        void onDialogEvent(Object event);
+    }
+    Set<Listener> mListener = new HashSet<>();
+    public final void registerListener(Listener listener) {
+        mListener.add(listener);
+    }
+    public final void unregisterListener(Listener listener) {
+        mListener.remove(listener);
+    }
+    public final Set<Listener> getListeners() {
+        return Collections.unmodifiableSet(mListener);
+    }
+    public void postEvent(Object event) {
+        for (Listener listener : getListeners()) {
+            listener.onDialogEvent(event);
+        }
+    }
     public DialogsManager(Context context, FragmentManager fragmentManager) {
         this.context = context;
         this.fragmentManager = fragmentManager;
     }
     public void showYesNoDialog(String title, String message) {
-        TwoOptionDialog dialog = TwoOptionDialog.newInstance(title, message, "Yes", "No");
+        TwoOptionDialog dialog = TwoOptionDialog.newInstance(this, title, message, "Yes", "No");
         dialog.show(fragmentManager, "yes_no_dialog");
     }
 }
