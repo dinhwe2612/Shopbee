d3391804be2f1da6d4a444a88e749a7fa7d050c1
dinhwe2612 <116061849+dinhwe2612@users.noreply.github.com>
2024-08-28 14:47:19 +0700

    add: product_detail layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -99,4 +99,7 @@ dependencies {
     // Add the dependency for the Realtime Database library
     // When using the BoM, you don't specify versions in Firebase library dependencies
     implementation("com.google.firebase:firebase-database")
+
+    // spark button
+    implementation("com.github.varunest:sparkbutton:1.0.6");
 }
