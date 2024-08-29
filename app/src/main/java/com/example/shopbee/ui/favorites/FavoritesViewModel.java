1a6a07e77f5c4363feb41e58f1ba488876ea591d
Mmyxxan <127069162+Mmyxxan@users.noreply.github.com>
2024-08-29 18:22:29 +0700

    favorites page created

--- app/src/main/java/com/example/shopbee/ui/favorites/FavoritesViewModel.java
+++ app/src/main/java/com/example/shopbee/ui/favorites/FavoritesViewModel.java
@@ -1,10 +1,40 @@
 package com.example.shopbee.ui.favorites;
 
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.MutableLiveData;
+
 import com.example.shopbee.data.Repository;
+import com.example.shopbee.data.model.api.AmazonProductByCategoryResponse;
 import com.example.shopbee.ui.common.base.BaseViewModel;
 
+import java.util.List;
+
 public class FavoritesViewModel extends BaseViewModel<FavoritesNavigator> {
+    MutableLiveData<List<AmazonProductByCategoryResponse.Data.Product>> favoriteProducts = new MutableLiveData<>();
+    MutableLiveData<List<String>> favoriteLists = new MutableLiveData<>();
     public FavoritesViewModel(Repository repository) {
         super(repository);
+    }
+
+    public MutableLiveData<List<String>> getFavoriteLists() {
+        return favoriteLists;
+    }
+
+    public MutableLiveData<List<AmazonProductByCategoryResponse.Data.Product>> getFavoriteProducts() {
+        return favoriteProducts;
+    }
+
+    public void getFavoriteVariationFromFirebase() {
+
+    }
+
+    public void syncFavoriteLists() {
+        // get users' favorite asins
+        favoriteLists.observe((LifecycleOwner) this, lists -> {
+            syncFavoriteProducts(favoriteLists.getValue());
+        });
+    }
+    public void syncFavoriteProducts(List<String> favoriteLists) {
+        // get information from Amazon asin
     }
 }
--- app/src/main/java/com/example/shopbee/ui/favorites/adapter/FavoriteAdapterGridView.java
+++ app/src/main/java/com/example/shopbee/ui/favorites/adapter/FavoriteAdapterGridView.java
@@ -1,4 +1,4 @@
-package com.example.shopbee.ui.search.adapter;
+package com.example.shopbee.ui.favorites.adapter;
 
 import android.graphics.Bitmap;
 import android.util.Log;
@@ -12,7 +12,9 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.FutureTarget;
 import com.example.shopbee.data.model.api.AmazonProductByCategoryResponse;
+import com.example.shopbee.databinding.FavoriteGridItemBinding;
 import com.example.shopbee.databinding.SaleItemBinding;
+import com.example.shopbee.ui.search.adapter.ProductAdapterGridView;
 
 import java.util.List;
 
@@ -21,17 +23,17 @@ import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.disposables.CompositeDisposable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
-public class ProductAdapterGridView extends RecyclerView.Adapter<ProductAdapterGridView.ViewHolder>{
+public class FavoriteAdapterGridView extends RecyclerView.Adapter<FavoriteAdapterGridView.ViewHolder> {
     public interface OnItemClickListener {
         void onItemClick(String asin);
     }
-    OnItemClickListener onItemClickListener;
-    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+    ProductAdapterGridView.OnItemClickListener onItemClickListener;
+    public void setOnItemClickListener(ProductAdapterGridView.OnItemClickListener onItemClickListener) {
         this.onItemClickListener = onItemClickListener;
     }
     private final CompositeDisposable compositeDisposable = new CompositeDisposable();
     private List<AmazonProductByCategoryResponse.Data.Product> products;
-    public ProductAdapterGridView(List<AmazonProductByCategoryResponse.Data.Product> products) {
+    public FavoriteAdapterGridView(List<AmazonProductByCategoryResponse.Data.Product> products) {
         this.products = products;
     }
     public void setProducts(List<AmazonProductByCategoryResponse.Data.Product> products) {
@@ -39,14 +41,14 @@ public class ProductAdapterGridView extends RecyclerView.Adapter<ProductAdapterG
     }
     @NonNull
     @Override
-    public ProductAdapterGridView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public FavoriteAdapterGridView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-        SaleItemBinding binding = SaleItemBinding.inflate(inflater, parent, false);
-        return new ProductAdapterGridView.ViewHolder(binding);
+        FavoriteGridItemBinding binding = FavoriteGridItemBinding.inflate(inflater, parent, false);
+        return new FavoriteAdapterGridView.ViewHolder(binding);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ProductAdapterGridView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull FavoriteAdapterGridView.ViewHolder holder, int position) {
         holder.bindView(position);
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,32 +72,24 @@ public class ProductAdapterGridView extends RecyclerView.Adapter<ProductAdapterG
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        private SaleItemBinding binding;
-        public ViewHolder(@NonNull SaleItemBinding binding) {
+        private FavoriteGridItemBinding binding;
+        public ViewHolder(@NonNull FavoriteGridItemBinding binding) {
             super(binding.getRoot());
             this.binding = binding;
         }
         public void bindView(int position) {
-            binding.textView1.setText(products.get(position).getProduct_title());
+            binding.itemFavoriteName.setText(products.get(position).getProduct_title());
             if (products.get(position).getProduct_star_rating() != null) {
                 binding.simpleRatingBar.setRating(Float.parseFloat(products.get(position).getProduct_star_rating()));
             } else {
                 binding.simpleRatingBar.setRating(0);
             }
-
-//            Log.e("rating", "msg: " + position + " " + products.get(position).getProduct_star_rating());
-            binding.textView3.setText("(" + products.get(position).getProduct_num_ratings() + ")");
-            if (products.get(position).getProduct_original_price() != null) {
-                binding.textView2.setText(products.get(position).getProduct_original_price());
-            }
-            else {
-                binding.textView2.setVisibility(View.GONE);
-                binding.imageView4.setVisibility(View.GONE);
-            }
-            binding.textView4.setText(products.get(position).getProduct_price());
+            binding.numRating.setText("(" + products.get(position).getProduct_num_ratings() + ")");
+            binding.favoriteItemPrice.setText(products.get(position).getProduct_price());
+            // variation
             if (products.get(position).getProduct_photo() != null) {
                 compositeDisposable.add(Observable.fromCallable(() -> {
-                                    FutureTarget<Bitmap> futureTarget = Glide.with(binding.imageView.getContext())
+                                    FutureTarget<Bitmap> futureTarget = Glide.with(binding.image.getContext())
                                             .asBitmap()
                                             .load(products.get(position).getProduct_photo())
                                             .submit();
@@ -104,7 +98,7 @@ public class ProductAdapterGridView extends RecyclerView.Adapter<ProductAdapterG
                                 .subscribeOn(Schedulers.io())
                                 .observeOn(AndroidSchedulers.mainThread())
                                 .subscribe(bitmap -> {
-                                    binding.imageView.setImageBitmap(bitmap);
+                                    binding.image.setImageBitmap(bitmap);
                                 }, throwable -> {
                                     Log.e("Exception", throwable.getMessage());
                                 })
