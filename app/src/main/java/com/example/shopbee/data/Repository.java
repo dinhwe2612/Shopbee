c706979713dcb5bd5ad3fa00f656c704ef4c3e65
minhluan85 <nmluan22@apcs.fitus.edu.vn>
2024-09-01 10:24:00 +0700

    Add variation to OrderResponse

--- app/src/main/java/com/example/shopbee/data/Repository.java
+++ app/src/main/java/com/example/shopbee/data/Repository.java
@@ -129,6 +129,8 @@ public class Repository {
                                     orderDetailResponseObj.setQuantity(orderDetailSnapshot.child("quantity").getValue(Integer.class));
                                     orderDetailResponseObj.setPrice(orderDetailSnapshot.child("price").getValue(String.class));
                                     orderDetailResponseObj.setUrlImage(orderDetailSnapshot.child("urlImage").getValue(String.class));
+                                    Map<String, String> variationMap = (Map<String, String>) orderDetailSnapshot.child("variation").getValue();
+                                    orderDetailResponseObj.setVariation(variationMap);
                                     orderDetailList.add(orderDetailResponseObj);
                                 }
                                 orderResponseObj.setOrder_detail(orderDetailList);
