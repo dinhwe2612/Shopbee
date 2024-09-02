aca3298028ac474d0e55cff565bd798f1024a1a7
minhluan85 <nmluan22@apcs.fitus.edu.vn>
2024-09-02 23:03:32 +0700

    add new layout: addNewCard

---
+++ app/src/main/java/com/example/shopbee/ui/checkout/adapter/PaymentAdapter.java
@@ -0,0 +1,79 @@
+package com.example.shopbee.ui.checkout.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.animsh.animatedcheckbox.AnimatedCheckBox;
+import com.example.shopbee.R;
+import com.example.shopbee.data.model.api.PaymentResponse;
+import com.example.shopbee.databinding.CountryItemBinding;
+import com.example.shopbee.databinding.PaymentItemBinding;
+import com.example.shopbee.ui.profile.adapter.CountryAdapter;
+
+import java.util.List;
+
+public class PaymentAdapter extends RecyclerView.Adapter<PaymentAdapter.PaymentViewHolder> {
+    private List<PaymentResponse> listPaymentResponse;
+    private int currentPosition;
+    public interface Listener{
+        void onClickItems(int position);
+    }
+    private Listener listener;
+    public PaymentAdapter(Listener listener, List<PaymentResponse> listPaymentResponse, int currentPosition) {
+        this.listener = listener;
+        this.listPaymentResponse = listPaymentResponse;
+        this.currentPosition = currentPosition;
+    }
+    @NonNull
+    @Override
+    public PaymentAdapter.PaymentViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        PaymentItemBinding binding = PaymentItemBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+        return new PaymentAdapter.PaymentViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PaymentAdapter.PaymentViewHolder holder, int position) {
+        PaymentResponse paymentResponse = listPaymentResponse.get(position);
+        switch (paymentResponse.getType()){
+            case "visa":
+                holder.binding.paymentIcon.setBackgroundResource(R.drawable.visa);
+                break;
+            case "master":
+                holder.binding.paymentIcon.setBackgroundResource(R.drawable.master);
+                break;
+        }
+        holder.binding.cardName.setText(paymentResponse.getName());
+        holder.binding.cardNumber.setText("* * * *  * * * *  * * * *  " + paymentResponse.getNumber().substring(12, 15));
+        holder.binding.expiryDate.setText(paymentResponse.getExpiryDate());
+        if (currentPosition == position){
+            holder.binding.checkBoxCard.setChecked(true);
+        } else {
+            holder.binding.checkBoxCard.setChecked(false);
+        }
+        holder.binding.checkBoxCard.setOnCheckedChangeListener(new AnimatedCheckBox.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(AnimatedCheckBox checkBox, boolean isChecked) {
+                notifyItemChanged(currentPosition);
+                currentPosition = position;
+                notifyItemChanged(currentPosition);
+                listener.onClickItems(position);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return listPaymentResponse.size();
+    }
+    public class PaymentViewHolder extends RecyclerView.ViewHolder {
+        PaymentItemBinding binding;
+        public PaymentViewHolder(@NonNull PaymentItemBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+}
--- app/src/main/java/com/example/shopbee/ui/checkout/payment/PaymentFragment.java
+++ app/src/main/java/com/example/shopbee/ui/checkout/payment/PaymentFragment.java
@@ -1,13 +1,29 @@
 package com.example.shopbee.ui.checkout.payment;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.databinding.library.baseAdapters.BR;
+import androidx.lifecycle.Observer;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.shopbee.R;
+import com.example.shopbee.data.model.api.ListOrderResponse;
+import com.example.shopbee.data.model.api.UserResponse;
 import com.example.shopbee.databinding.PaymentBinding;
 import com.example.shopbee.di.component.FragmentComponent;
+import com.example.shopbee.ui.checkout.adapter.PaymentAdapter;
 import com.example.shopbee.ui.common.base.BaseFragment;
 
-public class PaymentFragment extends BaseFragment<PaymentBinding, PaymentViewModel> implements PaymentNavigator {
+public class PaymentFragment extends BaseFragment<PaymentBinding, PaymentViewModel> implements PaymentNavigator, PaymentAdapter.Listener {
+    private PaymentBinding binding;
+    private UserResponse userResponse;
+    private ListOrderResponse listOrderResponse;
+    private PaymentAdapter paymentAdapter;
     @Override
     public int getBindingVariable() {
         return BR.vm;
@@ -22,5 +38,47 @@ public class PaymentFragment extends BaseFragment<PaymentBinding, PaymentViewMod
     public void performDependencyInjection(FragmentComponent buildComponent) {
         buildComponent.inject(this);
         viewModel.setNavigator(this);
+    }
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        binding = getViewDataBinding();
+        loadRealtimeData();
+        RecyclerView recyclerView = binding.recyclerView;
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        int positionDef = -1;
+        for (int i = 0; i < userResponse.getPayment().size(); i++) {
+            if (userResponse.getPayment().get(i).getDef()){
+                positionDef = i;
+            }
+        }
+        paymentAdapter = new PaymentAdapter(this, userResponse.getPayment(), positionDef);
+        recyclerView.setAdapter(paymentAdapter);
+        binding.buttonBackSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        binding.layoutWallet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        return binding.getRoot();
+    }
+
+    private void loadRealtimeData() {
+        viewModel.getUserResponse().observeForever(response -> userResponse = response);
+        viewModel.getOrderResponse().observeForever(response -> listOrderResponse = response);
+    }
+
+    @Override
+    public void onClickItems(int position) {
+        for (int i = 0; i < userResponse.getPayment().size(); i++) {
+            userResponse.getPayment().get(i).setDef(false);
+        }
+        userResponse.getPayment().get(position).setDef(true);
     }
 }
