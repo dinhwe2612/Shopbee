63638c94299b2fcc2b2dcd33006a1c0a38e43506
dinhwe2612 <116061849+dinhwe2612@users.noreply.github.com>
2024-09-02 17:29:53 +0700

    Add: Recommended Recycler into ProductDetailFragment

--- app/src/main/java/com/example/shopbee/ui/productdetail/adapter/RecommendedAdapter.java
+++ app/src/main/java/com/example/shopbee/ui/productdetail/adapter/RecommendedAdapter.java
@@ -1,5 +1,8 @@
 package com.example.shopbee.ui.productdetail.adapter;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -7,41 +10,90 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.FutureTarget;
+import com.example.shopbee.R;
 import com.example.shopbee.data.model.api.AmazonDealsResponse;
+import com.example.shopbee.data.model.api.AmazonProductByCategoryResponse;
+import com.example.shopbee.data.model.api.AmazonSearchResponse;
 import com.example.shopbee.databinding.DealItemBinding;
+import com.example.shopbee.databinding.RecommendedItemBinding;
+import com.example.shopbee.databinding.SaleItemBinding;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class RecommendedAdapter extends RecyclerView.Adapter<RecommendedAdapter.RecommendedViewHolder> {
-    List<AmazonDealsResponse.Data.Deal> deals = new ArrayList<>();
+    List<AmazonProductByCategoryResponse.Data.Product> products = new ArrayList<>();
+    Context context;
     final CompositeDisposable compositeDisposable = new CompositeDisposable();
-    public void setDeals(List<AmazonDealsResponse.Data.Deal> deals) {
-        this.deals = deals;
+    public RecommendedAdapter(Context context) {
+        this.context = context;
+    }
+    public void setProducts(List<AmazonProductByCategoryResponse.Data.Product> products) {
+        this.products = products;
         notifyDataSetChanged();
     }
     @NonNull
     @Override
     public RecommendedViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new RecommendedViewHolder(DealItemBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
+        return new RecommendedViewHolder(RecommendedItemBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecommendedViewHolder holder, int position) {
-        holder.binding.setDeal(deals.get(position));
+        compositeDisposable.add(Observable.fromCallable(() -> {
+                            FutureTarget<Bitmap> futureTarget = Glide.with(holder.binding.productImg.getContext())
+                                    .asBitmap()
+                                    .load(products.get(position).getProduct_photo())
+                                    .submit();
+                            return futureTarget.get();
+                        })
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(bitmap -> {
+                            holder.binding.productImg.setImageBitmap(bitmap);
+                        }, throwable -> {
+                            Log.e("error", throwable.getMessage());
+                        })
+        );
+        // bind title
+        String title = products.get(position).getProduct_title();
+        if (title.length() > 20) title = title.substring(0, 19) + "...";
+        holder.binding.title.setText(title);
+        // bind number of ratings
+        String numberOfRatings = products.get(position).getProduct_num_ratings();
+        if (numberOfRatings == null) numberOfRatings = "0";
+        holder.binding.numRatings.setText(numberOfRatings);
+        // bind rating bar
+        String rating = products.get(position).getProduct_star_rating();
+        if (rating == null) rating = "0";
+        holder.binding.ratingBar.setRating(Float.parseFloat(rating));
+        // bind price
+        holder.binding.curPrice.setText(products.get(position).getProduct_original_price());
+        String oldPrice = products.get(position).getProduct_original_price();
+        if (oldPrice == null) {
+            holder.binding.curPrice.setTextColor(context.getResources().getColor(R.color.black_background));
+            holder.binding.oldPrice.setVisibility(View.GONE);
+        } else {
+            holder.binding.oldPrice.setText(oldPrice);
+        }
     }
 
     @Override
     public int getItemCount() {
-        return deals.size();
+        return products.size();
     }
 
     public static class RecommendedViewHolder extends RecyclerView.ViewHolder {
-        DealItemBinding binding;
+        RecommendedItemBinding binding;
 
-        public RecommendedViewHolder(@NonNull DealItemBinding binding) {
+        public RecommendedViewHolder(@NonNull RecommendedItemBinding binding) {
             super(binding.getRoot());
             this.binding = binding;
         }
