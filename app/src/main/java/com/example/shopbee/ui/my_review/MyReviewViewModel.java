c23f621a5ceb3d64edcb315a57f135d88c706dc9
Mmyxxan <127069162+Mmyxxan@users.noreply.github.com>
2024-09-08 02:16:13 +0700

    my reviews done

--- app/src/main/java/com/example/shopbee/ui/my_review/MyReviewViewModel.java
+++ app/src/main/java/com/example/shopbee/ui/my_review/MyReviewViewModel.java
@@ -1,10 +1,39 @@
 package com.example.shopbee.ui.my_review;
 
+import android.util.Log;
+
+import androidx.lifecycle.MutableLiveData;
+
 import com.example.shopbee.data.Repository;
 import com.example.shopbee.ui.common.base.BaseViewModel;
+import com.example.shopbee.ui.common.dialogs.writereivewdialog.WriteReviewEvent;
+
+import java.util.List;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MyReviewViewModel extends BaseViewModel<MyReviewNavigator> {
+    MutableLiveData<List<WriteReviewEvent>> reviewList = new MutableLiveData<>();
+    public MutableLiveData<List<WriteReviewEvent>> getReviewList() {
+        return reviewList;
+    }
     public MyReviewViewModel(Repository repository) {
         super(repository);
+    }
+    public void syncReviewList() {
+        setIsLoading(true);
+        getCompositeDisposable().add(getRepository().getReviewsForUser()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(result -> {
+                            reviewList.setValue(result);
+                            setIsLoading(false);
+                        },
+                        error -> {
+                            setIsLoading(false);
+                            Log.e("syncReviewList", "syncReviewList: " + error.getMessage());
+                        })
+        );
     }
 }
---
+++ app/src/main/java/com/example/shopbee/ui/my_review/adapter/ImageAdapter.java
@@ -0,0 +1,44 @@
+package com.example.shopbee.ui.my_review.adapter;
+
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.shopbee.databinding.ImageReviewItemBinding;
+
+import java.util.List;
+
+public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder> {
+    private List<Bitmap> images;
+    public ImageAdapter(List<Bitmap> images) {
+        this.images = images;
+    }
+    @NonNull
+    @Override
+    public ImageAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(ImageReviewItemBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ImageAdapter.ViewHolder holder, int position) {
+        holder.binding.image.setImageBitmap(images.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return images.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageReviewItemBinding binding;
+        public ViewHolder(@NonNull ImageReviewItemBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+}
