ed68f0291a58007e52d9d61ad14932a40e393ad1
minhluan85 <nmluan22@apcs.fitus.edu.vn>
2024-09-08 10:56:48 +0700

    hi

--- app/src/main/java/com/example/shopbee/ui/profile/myorder/MyOrderDetailFragment.java
+++ app/src/main/java/com/example/shopbee/ui/profile/myorder/MyOrderDetailFragment.java
@@ -185,7 +185,6 @@ public class MyOrderDetailFragment extends BaseFragment<OrderDetailsBinding, MyO
                                                 .show();
                                         dialog.dismiss();
                                     }
-
                                     @Override
                                     public void onNegativeButtonClicked(Dialog dialog) {
                                         dialog.dismiss();
@@ -255,7 +254,7 @@ public class MyOrderDetailFragment extends BaseFragment<OrderDetailsBinding, MyO
     @Override
     public void reorder(OrderResponse orderResponse) {
         NavController navController = NavHostFragment.findNavController(this);
-        Bundle bundle = new Bundle();
+        Bundle bundle = new Bundle(); 
         bundle.putParcelable("orderResponse", orderResponse);
         navController.navigate(R.id.checkoutFragment, bundle);
     }
